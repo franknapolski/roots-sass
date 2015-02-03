9259a394c81a332e73eb829b529beffd6dc36e45
Frank Napolski <frank.napolski@gmail.com>
2015-02-02 20:34:26 -0500

    .

--- lib/init.php
+++ lib/init.php
@@ -46,8 +46,26 @@ function roots_widgets_init() {
   ));
 
   register_sidebar(array(
-    'name'          => __('Footer', 'roots'),
-    'id'            => 'sidebar-footer',
+    'name'          => __('Footer Left', 'roots'),
+    'id'            => 'sidebar-footer-left',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>',
+  ));
+
+  register_sidebar(array(
+    'name'          => __('Footer Middle', 'roots'),
+    'id'            => 'sidebar-footer-middle',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>',
+  ));
+
+  register_sidebar(array(
+    'name'          => __('Footer Right', 'roots'),
+    'id'            => 'sidebar-footer-right',
     'before_widget' => '<section class="widget %1$s %2$s">',
     'after_widget'  => '</section>',
     'before_title'  => '<h3>',
--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,10 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="row">
+    	<div class="col-sm-4"><?php dynamic_sidebar('sidebar-footer-left'); ?></div>
+    	<div class="col-sm-4"><?php dynamic_sidebar('sidebar-footer-middle'); ?></div>
+    	<div class="col-sm-4"><?php dynamic_sidebar('sidebar-footer-right'); ?></div>
+    </div>
+
   </div>
 </footer>
